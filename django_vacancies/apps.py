ec4f7f878d08926c078166c33ecd10d43f649542
Razy070 <rdimovin@mail.ru>
2023-04-26 15:44:54 +0700

    Revert "гз"
    This reverts commit 55301fb9e179828f71240da3804dfbdf27b75055.

--- django_vacancies/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoVacanciesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_vacancies'
-    verbose_name = "Приложение для публикации и просмотра вакансий"
